be2e162ff1d710a11b0c044401c74c80be38278c
yeger00 <yeger00@gmail.com>
2025-10-22 15:06:01 +0300

    Add another bank

---
+++ src/memory/bank_other.ts
@@ -0,0 +1,2 @@
+// Taken from: https://supabase.com/blog/in-browser-semantic-search-pglite
+export const memoryBankOther = ["Bed","Car","Train","Cat","Dog","Apple","Boat","Mouse","Chair","Tomato","Desk","Banana"];
--- src/memory/banks.ts
+++ src/memory/banks.ts
@@ -1,11 +1,13 @@
 import { memoryBankGeneral } from './bank_general';
 import { memoryBankProgramming } from './bank_programming';
 import { memoryBankScience } from './bank_science';
+import { memoryBankOther } from './bank_other';
 
-export type MemoryBankName = "General" | "Programming" | "Science";
+export type MemoryBankName = "General" | "Programming" | "Science" | "Other";
 
 export const memoryBanks: Record<MemoryBankName, string[]> = {
   General: memoryBankGeneral,
   Programming: memoryBankProgramming,
   Science: memoryBankScience,
+  Other: memoryBankOther,
 }; 
